7f38b2f20a4cf7369d750479133c3cd4f4117189
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-09-05 16:34:26 -0400

    adds UMD wrapper

--- cycletext.js
+++ cycletext.js
@@ -1,5 +1,32 @@
-(function cycleText($){
-  $.fn.cycleText = function(selector, quotes, options){
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = function( root, jQuery ) {
+      if ( jQuery === undefined ) {
+        // require('jQuery') returns a factory that requires window to
+        // build a jQuery instance, we normalize how we use modules
+        // that require this pattern but the window provided is a noop
+        // if it's defined (how jquery works)
+        if ( typeof window !== 'undefined' ) {
+          jQuery = require('jquery');
+        }
+        else {
+          jQuery = require('jquery')(root);
+        }
+      }
+      factory(jQuery);
+      return jQuery;
+    };
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+}(function ($) {
+  $.fn.cycleText = function (selector, quotes, options){
     var self = this;
     if (typeof selector === "object"){
       options = quotes;
@@ -121,4 +148,11 @@
     displayQuote();
   };
   return this;
-}(window.jQuery));
+}));
+
+
+
+
+// (function cycleText($){
+//   $.fn.cycleText = function
+
